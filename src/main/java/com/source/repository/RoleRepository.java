c653e5f142f32deb51bf297942d4052788726084
Ruslan Klimchuk <klimatwua@gmail.com>
2023-10-14 21:04:20 +0300

    Basic authorization structure and database

---
+++ src/main/java/com/source/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.source.repository;
+
+
+import com.source.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/com/source/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.source.repository;
+
+import com.source.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
