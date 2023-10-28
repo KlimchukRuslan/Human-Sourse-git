8aa3d2293ee11f88fc4da575fd93d3f07bc1ac6b
Ruslan Klimchuk <klimatwua@gmail.com>
2023-10-28 21:45:46 +0300

    Add and revrite property for database

--- src/main/java/com/source/Application.java
+++ src/main/java/com/source/Application.java
@@ -2,8 +2,11 @@ package com.source;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
 
 @SpringBootApplication
+@PropertySource("classpath:database.properties")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
